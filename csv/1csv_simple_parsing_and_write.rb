8399580ec8b44850161d7f0331ca2b3448ec7e57
Kozo Nishida <kozo.nishida@gmail.com>
2016-11-14 05:00:37 +0900

    Update 1csv_simple_parsing_and_write.rb

--- csv/1csv_simple_parsing_and_write.rb
+++ csv/1csv_simple_parsing_and_write.rb
@@ -1,15 +1,18 @@
 input_file = ARGV[0]
-#output_file = ARGV[1]
+output_file = ARGV[1]
 
-File.open(input_file, 'r') do |filereader|
-  p filereader.class
-  header = filereader.readline
-  header = header.strip
-  header_list = header.split(',')
-  p header_list
-  filereader.each do |row|
-    row = row.strip()
-    row_list = row.split(',')
-    p row_list
+File.open(input_file, "r") do |filereader|
+  File.open(output_file, "w") do |filewriter|
+    header = filereader.readline
+    header = header.strip
+    header_list = header.split(",")
+    p header_list
+    filewriter.write(header_list.join(',')+"\n")
+    filereader.each do |row|
+      row = row.strip()
+      row_list = row.split(",")
+      p row_list
+      filewriter.write(row_list.join(",")+"\n")
+    end
   end
 end
